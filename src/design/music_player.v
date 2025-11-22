e432b81b09b43ab04df98a9cceb94d801625f2b4
alejandroolveracano <aolvera1@stanford.edu>
2025-11-21 18:28:14 -0800

    Update music_player.v

--- src/design/music_player.v
+++ src/design/music_player.v
@@ -1,154 +1,59 @@
-//
-//  music_player module
-//
-//  This music_player module connects up the MCU, song_reader, note_player,
-//  beat_generator, and codec_conditioner. It provides an output that indicates
-//  a new sample (new_sample_generated) which will be used in lab 5.
-//
-
-module music_player(
-    // Standard system clock and reset
+module note_player(
     input clk,
     input reset,
-
-    // Our debounced and one-pulsed button inputs.
-    input play_button,
-    input next_button,
-
-    // The raw new_frame signal from the ac97_if codec.
-    input new_frame,
-
-    // This output must go high for one cycle when a new sample is generated.
-    output wire new_sample_generated,
-    
-    output wire [1:0] state,     // for state_symbol module
-
-    output wire [5:0] prev_note,  // The previous note played (for note display module)
-    output wire [5:0] note,       // The current note being played (for note display module)
-    
-    // Our final output sample to the codec. This needs to be synced to
-    // new_frame.
-    output wire [15:0] sample_out
+    input play_enable,  // When high we play, when low we don't.
+    input [5:0] note_to_load,  // The note to play
+    input [5:0] duration_to_load,  // The duration of the note to play
+    input load_new_note,  // Tells us when we have a new note to load
+    output done_with_note,  // When we are done with the note this stays high.
+    input beat,  // This is our 1/48th second beat
+    input generate_next_sample,  // Tells us when the codec wants a new sample
+    output [15:0] sample_out,  // Our sample output
+    output new_sample_ready,  // Tells the codec when we've got a sample
+    output [5:0] prev_note,  // The previous note played (for note display module)****************
+    output [5:0] note  // The current note being played (for note display module)
 );
-    // The BEAT_COUNT is parameterized so you can reduce this in simulation.
-    // If you reduce this to 100 your simulation will be 10x faster.
-    parameter BEAT_COUNT = 1000;
 
-
-//
-//  ****************************************************************************
-//      Master Control Unit
-//  ****************************************************************************
-//   The reset_player output from the MCU is run only to the song_reader because
-//   we don't need to reset any state in the note_player. If we do it may make
-//   a pop when it resets the output sample.
-//
- 
-    wire play;
-    wire reset_player;
-    wire [1:0] current_song;
-    wire song_done;
-    mcu mcu(
-        .clk(clk),
-        .reset(reset),
-        .play_button(play_button),
-        .next_button(next_button),
-        .play(play),
-        .reset_player(reset_player),
-        .song(current_song),
-        .state(state),
-        .song_done(song_done)
-    );
-
-//
-//  ****************************************************************************
-//      Song Reader
-//  ****************************************************************************
-//
-    wire [5:0] note_to_play;
-    wire [5:0] duration_for_note;
-    wire new_note;
-    wire note_done;
-    song_reader song_reader(
-        .clk(clk),
-        .reset(reset | reset_player),
-        .play(play),
-        .song(current_song),
-        .song_done(song_done),
-        .note(note_to_play),
-        .duration(duration_for_note),
-        .new_note(new_note),
-        .note_done(note_done)
-    );
-
-//   
-//  ****************************************************************************
-//      Note Player
-//  ****************************************************************************
-//  
-    wire beat;
-    wire generate_next_sample, generate_next_sample0;
-    wire [15:0] note_sample, note_sample0;
-    wire note_sample_ready, note_sample_ready0;
+    // Implementation goes here!
     
+    //beat_generator bg (.clk(), .reset(), .en(), .beat()); INSTANTIATE THIS IN HIGHER MODULE
+    
+    // duration registers
+    wire [5:0] duration;
+    wire [5:0] next_duration;
+    wire duration_en = (beat | load_new_note) & play_enable;    // logic for duration enable                                                            
+    dffre #(6) duration_reg (.clk(clk), .r(reset), .en(duration_en), .d(next_duration), .q(duration));
+    
+    // note registers
+    wire [5:0] next_note;
+    dffr #(6) note_reg (.clk(clk), .r(reset), .d(next_note), .q(note));
 
-
-    // These pipeline registers were added to decrease the length of the critical path!
-    dffr pipeline_ff_gen_next_sample (.clk(clk), .r(reset), .d(generate_next_sample0), .q(generate_next_sample));
-    dffr #(.WIDTH(16)) pipeline_ff_note_sample (.clk(clk), .r(reset), .d(note_sample0), .q(note_sample));
-    dffr pipeline_ff_new_sample_ready (.clk(clk), .r(reset), .d(note_sample_ready0), .q(note_sample_ready));
-
-    note_player note_player(
-        .clk(clk),
-        .reset(reset),
-        .play_enable(play),
-        .note_to_load(note_to_play),
-        .duration_to_load(duration_for_note),
-        .load_new_note(new_note),
-        .done_with_note(note_done),
-        .beat(beat),
-        .generate_next_sample(generate_next_sample),
-        .sample_out(note_sample0),
-        .new_sample_ready(note_sample_ready0),
-        .prev_note(prev_note),
-        .note(note)
-    );
-      
-//   
-//  ****************************************************************************
-//      Beat Generator
-//  ****************************************************************************
-//  By default this will divide the generate_next_sample signal (48kHz from the
-//  codec's new_frame input) down by 1000, to 48Hz. If you change the BEAT_COUNT
-//  parameter when instantiating this you can change it for simulation.
-//  
-    beat_generator #(.WIDTH(10), .STOP(BEAT_COUNT)) beat_generator(
-        .clk(clk),
-        .reset(reset),
-        .en(generate_next_sample),
-        .beat(beat)
-    );
-
-//  
-//  ****************************************************************************
-//      Codec Conditioner
-//  ****************************************************************************
-//  
-    wire new_sample_generated0;
-    wire [15:0] sample_out0; 
-
-    dffr pipeline_ff_nsg (.clk(clk), .r(reset), .d(new_sample_generated0), .q(new_sample_generated));
-    dffr #(.WIDTH(16)) pipeline_ff_sample_out (.clk(clk), .r(reset), .d(sample_out0), .q(sample_out));
-
-    assign new_sample_generated0 = generate_next_sample;
-    codec_conditioner codec_conditioner(
-        .clk(clk),
-        .reset(reset),
-        .new_sample_in(note_sample),
-        .latch_new_sample_in(note_sample_ready),
-        .generate_next_sample(generate_next_sample0),
-        .new_frame(new_frame),
-        .valid_sample(sample_out0)
-    );
-
+    dffre #(6) display_note_reg (.clk(clk), .r(reset), .en(load_new_note), .d(note), .q(prev_note));     // added for note display module
+    
+    assign done_with_note = (duration == 6'd0) & beat;
+    
+    // note mux
+    wire note_sel = load_new_note;
+    assign next_note = (note_sel) ? note_to_load : note;
+    
+    // duration mux
+    wire duration_sel = (load_new_note | reset | done_with_note);
+    assign next_duration = (duration_sel) ? duration_to_load : (duration - 6'd1);
+    
+    
+    // Frequency ROM
+    wire [19:0] step_size;
+    frequency_rom f_rom (.clk(clk), .addr(note), .dout(step_size));
+    
+    wire [15:0] sine_reader_out;
+    
+    // Sine Reader
+    sine_reader sr (
+        .clk(clk), 
+        .reset(reset), 
+        .step_size(step_size), 
+        .generate_next(generate_next_sample), 
+        .sample_ready(new_sample_ready), 
+        .sample(sine_reader_out));
+    assign sample_out = play_enable ? sine_reader_out : 16'd0;
 endmodule
